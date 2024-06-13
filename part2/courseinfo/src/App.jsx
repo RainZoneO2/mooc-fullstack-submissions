c8e4b4a4b9ebe132d446319ed50f061c00897722
Ghaith A <alghaith.ah38@gmail.com>
2024-06-13 15:10:32 -0400

    Setup course info project. Refactored to allow for arbitrary number of course parts. Refactored with destructuring assignments. Step 6

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,65 @@
+const Header = ({course}) => <h1>{course}</h1>
+
+const Part = ({part}) => {
+  return (
+    <li> {part.name} {part.exercises} </li>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      <ul>
+        {parts.map(part =>     
+            <Part key={part.id} part={part} />
+        )}
+      </ul>      
+    </div>
+  )
+}
+
+// const Total = ({parts}) => {
+//   return (
+//     <>
+//       <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+//     </>
+//   )
+// }
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      {/* <Total parts={course.parts} /> */}
+  </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [ 
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  
+  return <Course course={course} />
+}
+
+export default App
